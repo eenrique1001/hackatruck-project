d3ecba926a470897284d45737da3e92285b2a153
Ruan <ruanpablooliveira20@gmail.com>
2025-02-27 09:02:45 -0300

    Initial Commit

---
+++ teste1/teste1App.swift
@@ -0,0 +1,17 @@
+//
+//  teste1App.swift
+//  teste1
+//
+//  Created by Turma01-11 on 27/02/25.
+//
+
+import SwiftUI
+
+@main
+struct teste1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
