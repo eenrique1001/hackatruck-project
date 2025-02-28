5d68edc29de84ab5637e1b0b561dfcc807bc248a
Athina <athinags@hotmail.com>
2025-02-28 11:10:33 -0300

    comeco da tela de reserva emergencial mas nao sei ta bem confuso

--- Doubloons/telaReservaEmergencial.swift
+++ Doubloons/telaReservaEmergencial.swift
@@ -1,18 +1,101 @@
 //
-//  telaReservaEmergencial.swift
-//  Doubloons
+//  telaGastos.swift
+//  telaGastos
 //
-//  Created by Turma01-11 on 28/02/25.
+//  Created by Turma01-11 on 27/02/25.
 //
 
 import SwiftUI
 
 struct telaReservaEmergencial: View {
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            HStack{
+                Image(systemName: "house")
+                    .resizable()
+                    .frame(width: 40, height: 30)
+                    .imageScale(.large)
+                    .foregroundStyle(.white)
+                Text("")
+                Text("Início")
+                    .foregroundStyle(.white)
+                    .font(.title2)
+            }.frame(width: 150, height: 50).clipShape(Rectangle()).background(LinearGradient(gradient: Gradient(colors:[ .black, .green]),startPoint: .leading, endPoint: .trailing))
+                .cornerRadius(20.0)
+                .padding(.leading,-205)
+                .shadow(radius: 5)
+                .offset(x: -10, y: -2)
+            
+            Spacer().frame(height: 28)
+            
+            VStack{
+                HStack{
+                    Text("Reserva Emergencial")
+                        .font(.title)
+                        .bold()
+                        .padding(.top, 20)
+                        .padding(.leading, 20)
+                        .padding(.bottom, 5)
+                    Spacer()
+                    Image(systemName: "line.3.horizontal")
+                        .resizable()
+                        .frame(width: 40, height: 20)
+                        .foregroundStyle(.white)
+                        .padding()
+                }
+                HStack{
+                    HStack{
+                        Text("R$ 522,04")
+                            .font(.title3)
+                            .bold()
+                            .padding(.leading, 20)
+                    }
+                    .padding(.bottom, 10)
+                    Spacer()
+                }
+                .padding(.bottom, 10)
+            }
+            .background(LinearGradient(gradient: Gradient(colors: [Color.green, Color.black]), startPoint: .topTrailing, endPoint: .bottomLeading))
+            .foregroundColor(.white)
+            .clipShape(UnevenRoundedRectangle(cornerRadii: .init(topLeading: 20, bottomLeading: 20)))
+            .shadow(radius: 10)
+            .padding(.leading, 15)
+            
+            Spacer().frame(height: 30)
+            Text("")
+            
+            HStack{
+                Text("Criar")
+                    .fontWeight(.semibold)
+                    .foregroundStyle(.white)
+            }.frame(width: 150, height: 60)
+                .background(LinearGradient(gradient: Gradient(colors:[.green, .black]),startPoint: .topTrailing, endPoint: .bottomLeading))
+                .clipShape(UnevenRoundedRectangle(cornerRadii: .init(bottomLeading: 10, topTrailing: 10)))
+                .shadow(radius: 8)
+            
+            Spacer().frame(height: 40)
+            
+            HStack{
+                Spacer().frame(width: 50)
+                Text("Saldo Restante Mensal")
+                    .foregroundStyle(.white)
+                    .fontWeight(.semibold)
+                
+                Spacer().frame(width: 45)
+                
+                Text("$522,04")
+                    .foregroundStyle(.white)
+                    .fontWeight(.semibold)
+                
+            }.frame(width: 370, height: 60)
+                .background(.cinzaescuro)
+                .clipShape(UnevenRoundedRectangle(cornerRadii: .init(bottomTrailing: 10, topTrailing: 10)))
+                .shadow(radius: 8)
+                .offset(x: -65)
+        }
     }
 }
-
 #Preview {
     telaReservaEmergencial()
 }
