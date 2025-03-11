2e9881a94f4041236618ffbc278f16cce318bbab
eenrique1001 <ezioenrique94@gmail.com>
2025-03-11 08:56:34 -0300

    funcao post iniciada

--- Doubloons/ViewModel.swift
+++ Doubloons/ViewModel.swift
@@ -25,5 +25,52 @@ class FinanceiroViewModel : ObservableObject {
         
         task.resume()
     }
+    
+    func post(_ obj : Financeiro){
+        
+        
+        guard let url = URL(string: "http://192.168.128.90:1880/postD") else { return }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        
+        // Headers must specify that the HTTP request body is JSON-encoded
+        var headers = request.allHTTPHeaderFields ?? [:]
+        headers["Content-Type"] = "application/json"
+        request.allHTTPHeaderFields = headers
+        
+        let encoder = JSONEncoder()
+        
+        do {
+            let jsonData = try encoder.encode(obj)
+            request.httpBody = jsonData
+            print("jsonData: ", String(data: request.httpBody!, encoding: .utf8) ?? "no body data")
+        } catch {
+            print("Error encoding to JSON: \(error.localizedDescription)")
+        }
+        
+        
+        
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            if let error = error {
+                print("Error to send resource: \(error.localizedDescription)")
+                return
+            }
+            
+            guard let httpResponse = response as? HTTPURLResponse else {
+                print("Error to send resource: invalid response")
+                return
+            }
+            
+            if httpResponse.statusCode == 200 {
+                print("Resource POST successfully")
+            } else {
+                print("Error POST resource: status code \(httpResponse.statusCode)")
+            }
+        }
+        
+        task.resume()
+        
+    }
 }
 
